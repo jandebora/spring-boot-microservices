99fb4a9af51fb251b44bb15baca48ab28f71b931
Carlos Cagigao Bravo <ccagigao@atsistemas.com>
2019-02-22 14:03:15 +0100

    Fix user privileges error

--- sql-scripts/init-database.sql
+++ sql-scripts/init-database.sql
@@ -2,9 +2,11 @@
 CREATE SCHEMA users;
 CREATE SCHEMA cards;
 -- Creating users
-CREATE USER 'dbusers'@'localhost' IDENTIFIED BY 'dbuserspassword';
-CREATE USER 'dbcards'@'localhost' IDENTIFIED BY 'dbcardspassword';
+CREATE USER 'dbusers'@'%' IDENTIFIED BY 'dbuserspassword';
+CREATE USER 'dbcards'@'%' IDENTIFIED BY 'dbcardspassword';
 -- Giving appropriate access
-GRANT ALL PRIVILEGES ON users. * TO 'dbusers'@'localhost';
-GRANT ALL PRIVILEGES ON cards. * TO 'dbcards'@'localhost';
+GRANT ALL PRIVILEGES ON users.* TO 'dbusers'@'%';
+GRANT ALL PRIVILEGES ON cards.* TO 'dbcards'@'%';
+-- REFRESH
+FLUSH PRIVILEGES;
 
